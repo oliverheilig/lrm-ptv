b14ce58435decc9cfca382d46732ec801f001572
PersonalAccessToken <oliver.heilig@yahoo.de>
2022-06-26 19:37:48 +0200

    initial release

--- apikey.js
+++ apikey.js
@@ -1,2 +1,2 @@
-// This is my personal api key. Please get your own free key at https://developer.myptv.com/
-var apiKey = 'YjA2OGIzOGMxMjdiNGZkOGIwYTMwNmI0OGE4Y2ZiMTc6Y2QwZjUyMDYtMTk2Ni00N2NlLWFmNTYtNzBjOWZjZTgxNzJh';
+// Get your own free key at https://developer.myptv.com/
+var apiKey = '';

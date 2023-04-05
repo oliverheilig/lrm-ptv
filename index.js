bf35649d2c1e24fc65cdb61e3b6d419771b9f86a
Oliver Heilig <oliver.heilig@yahoo.de>
2023-04-05 17:06:32 +0200

    Filter options

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var clusterName = 'xserver2-test';
-var scenario = 'eu';
+var scenario = 'EUR';
 var routingProfile = 'EUR_CAR';
 var useImperial = false;
 
 var baseLayers;
 var routingControl;
+var optionsCache = [];
 
 // initialize the map
 var map = L.map('map', {
@@ -43,34 +44,34 @@ map.getPane('tileOverlayPane').style.pointerEvents = 'none';
 // get the start and end coordinates for a scenario
 var getPlan = function () {
 	switch (scenario) {
-	case 'na':
-	{
-		return [
-			L.latLng(40.71454, -74.00711),
-			L.latLng(42.35867, -71.05672)
-		];
-	}
-	case 'au':
-	{
-		return [
-			L.latLng(-33.86959, 151.20694),
-			L.latLng(-35.3065, 149.12659)
-		];
-	}
-	case 'imea':
-	{
-		return [
-			L.latLng(30.056111, 31.239444),
-			L.latLng(25.266667, 55.3)
-		];
-	}
-	default:
-	{ // 'eu'	
-		return [
-			L.latLng(48.8588, 2.3469),
-			L.latLng(52.3546, 4.9039)
-		];
-	}
+		case 'USA':
+			{
+				return [
+					L.latLng(40.71454, -74.00711),
+					L.latLng(42.35867, -71.05672)
+				];
+			}
+		case 'AUS':
+			{
+				return [
+					L.latLng(-33.86959, 151.20694),
+					L.latLng(-35.3065, 149.12659)
+				];
+			}
+		case 'IMEA':
+			{
+				return [
+					L.latLng(30.056111, 31.239444),
+					L.latLng(25.266667, 55.3)
+				];
+			}
+		default:
+			{ // 'EUR'	
+				return [
+					L.latLng(48.8588, 2.3469),
+					L.latLng(52.3546, 4.9039)
+				];
+			}
 	}
 };
 
@@ -98,8 +99,7 @@ var initializeRoutingControl = function () {
 			},
 			geocoder: L.Control.Geocoder.ptv({
 				serviceUrl: 'https://api.myptv.com',
-				apiKey: apiKey,
-				profile: routingProfile
+				apiKey: apiKey
 			}),
 			reverseWaypoints: true
 		}),
@@ -127,7 +127,8 @@ var initializeRoutingControl = function () {
 		},
 		router: L.Routing.ptv({
 			serviceUrl: 'https://api.myptv.com/',
-			apiKey: apiKey
+			apiKey: apiKey,
+			profile: routingProfile
 		}),
 		collapsible: true,
 		routeWhileDragging: false,
@@ -137,7 +138,7 @@ var initializeRoutingControl = function () {
 		})
 	}).addTo(map);
 
-	routingControl.on('routingerror', function (e) {});
+	routingControl.on('routingerror', function (e) { });
 
 	L.Routing.errorControl(routingControl).addTo(map);
 };
@@ -166,16 +167,52 @@ L.control.layers(baseLayers, {}, {
 }).addTo(map);
 
 var _onMapLoad = function (e) {
+	// Load cache
+	var sel = document.getElementById('vehicleType');
+	for (var i = 0, iLen = sel.options.length; i < iLen; i++) {
+		optionsCache.push(sel.options[i]);
+	}
+
+	filterItems("EUR_")
+
 	initializeRoutingControl();
 };
 
 map.on('load', _onMapLoad, this);
 map.setView([0, 0], 1);
 
+function filterItems(prefix) {
+	var sel = document.getElementById('vehicleType');
+	
+	restoreOptions();
+
+	// Loop backwards through options as removing them modifies the next
+	// to be visited if go forwards
+	for (var i = sel.options.length - 1; i >= 0; i--) {
+		opt = sel.options[i];
+		if (opt.text.indexOf(prefix) == -1) {
+			sel.removeChild(opt)
+		}
+	}
+
+	sel.options[0].selected = "selected";
+}
+
+// Restore select to original state
+function restoreOptions() {
+	var sel = document.getElementById('vehicleType');
+	sel.options.length = 0;
+	for (var i = 0, iLen = optionsCache.length; i < iLen; i++) {
+		sel.appendChild(optionsCache[i]);
+	}
+}
 
 // update the map scenario
 var updateScenario = function () {
 	scenario = $('#scenarioSelect option:selected').val();
+
+	filterItems(scenario + "_");
+
 	updateParams(true);
 };
 
@@ -184,11 +221,10 @@ var updateParams = function (updateWayPoints) {
 	routingProfile = $('#vehicleType').val();
 	useImperial = $('#useImperial').is(':checked');
 
-	if (updateWayPoints)
-	{ 
+	if (updateWayPoints) {
 		routingControl.setWaypoints(getPlan());
 	}
-	
+
 	routingControl._router.options.profile = routingProfile;
 	routingControl._formatter.options.units = useImperial ? 'imperial' : 'metric';
 	routingControl.route();

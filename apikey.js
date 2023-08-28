be2d5af835039d4d5be917216011c77a13196a1c
Oliver Heilig <oliver.heilig@yahoo.de>
2023-08-28 20:23:51 +0200

    Update apikey.js

--- apikey.js
+++ apikey.js
@@ -1,2 +1,2 @@
-// Get your own free key at https://developer.myptv.com/
-var apiKey = '';
+// This is my personal api key. Please get your own free key at https://developer.myptv.com/
+var apiKey = 'YjA2OGIzOGMxMjdiNGZkOGIwYTMwNmI0OGE4Y2ZiMTc6Y2QwZjUyMDYtMTk2Ni00N2NlLWFmNTYtNzBjOWZjZTgxNzJh';
